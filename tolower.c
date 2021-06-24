0b33980f2d76800a3c2bcdc6f878660d990a85df
ubuntu1804 <jcwccu@gmail.com>
2021-06-24 14:21:26 +0800

    ver1

---
+++ tolower.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define MaxBuf 1 << 18
+
+int main(int argc, char **argv) {
+    char *buf;
+    int fd;
+    unsigned int fileSize = 0;
+    char *ptr;
+
+    fd = open(argv[1], O_RDWR);
+    fileSize = lseek(fd, 0, SEEK_END);
+    buf = (char*) malloc(fileSize + 1);
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buf, fileSize);
+    ptr = buf;
+
+    while(*ptr) {
+        if(*ptr >= 'A' && *ptr <= 'Z')    *ptr = *ptr + 32;
+        ptr++;
+    }
+    buf[fileSize] = '\0';
+    lseek(fd, 0, SEEK_SET);
+    write(fd, buf, fileSize);
+} 
